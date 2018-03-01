c00d639d9060ba4260d2f1af3191638401c229ea
Duncan McAlester <duncan@rocketcom.com>
2018-03-01 09:29:57 -0800

    pause to work on buttons

--- build/default/src/astro-components/rux-component/rux-component.js
+++ build/default/src/astro-components/rux-component/rux-component.js
@@ -5,22 +5,20 @@ import { html } from "/node_modules/@polymer/polymer/polymer-element.js";
  * @polymer
  * @extends HTMLElement
  */
-export class AstroModems extends PolymerElement {
+export class RuxComponent extends PolymerElement {
   static get properties() {
     return {
-      modems: {
-        type: Object
-      }
+      min: String,
+      max: String,
+      value: String,
+      text: String
     };
   }
 
   static get template() {
     return html`
-      <link rel="stylesheet" href="/src/astro-app/astro-modems/astro-modems.css">
-      <slot id="list"></slot>
-      <div>Test[[modems]]</div>
-      <!-- <slot id="detail"></slot> //-->
-    `;
+      <link rel="stylesheet" href="/src/astro-components/rux-component/rux-component.css">
+      <div class="rux-component">This is a blank component template written in Polymer 3. Use me as a starting point for a new Component</div>`;
   }
 
   constructor() {
@@ -40,4 +38,4 @@ export class AstroModems extends PolymerElement {
   }
 }
 
-customElements.define("astro-modems", AstroModems);
+customElements.define("rux-component", RuxComponent);
