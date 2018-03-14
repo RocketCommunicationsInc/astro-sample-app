557620340109a1782b3b2048098c5c58667ae24d
Duncan McAlester <duncan@rocketcom.com>
2018-03-14 12:32:36 -0700

    Needed(?) to make rux-timeline-track a MutableObject to observe the sub-property changes on timeline regions

--- src/astro-components/rux-timeline/rux-timeline-track.js
+++ src/astro-components/rux-timeline/rux-timeline-track.js
@@ -1,4 +1,5 @@
 import { Element as PolymerElement } from "/node_modules/@polymer/polymer/polymer-element.js";
+import { MutableData } from "/node_modules/@polymer/polymer/lib/mixins/mutable-data.js";
 import { html } from "/node_modules/@polymer/polymer/polymer-element.js";
 import { RuxTimelineRegion } from "./rux-timeline-region.js";
 import "/node_modules/@polymer/polymer/lib/elements/dom-repeat.js";
@@ -7,7 +8,7 @@ import "/node_modules/@polymer/polymer/lib/elements/dom-repeat.js";
  * @polymer
  * @extends HTMLElement
  */
-export class RuxTimelineTrack extends PolymerElement {
+export class RuxTimelineTrack extends MutableData(PolymerElement) {
   static get properties() {
     return {
       label: {
@@ -32,24 +33,20 @@ export class RuxTimelineTrack extends PolymerElement {
       <div class="rux-timeline__track">
         <div class="rux-timeline__track__label">[[label]]</div>
         <ol>
-        
           <template is="dom-repeat" id="regionsElements" items={{regions}} mutable-data>
-            
-              <li>
-                <rux-timeline-region class="rux-timeline-region"
-                  title=[[item.label]]
-                  status=[[item.status]]
-                  start-time=[[item.startTime]]
-                  end-time=[[item.endTime]]
-                  left=[[item.left]]
-                  width=[[item.width]]>
-                </rux-timeline-region>
-              </li>
-            </template>
-          
+            <li>
+              <rux-timeline-region class="rux-timeline-region"
+                title=[[item.label]]
+                status=[[item.status]]
+                start-time=[[item.startTime]]
+                end-time=[[item.endTime]]
+                left=[[item.left]]
+                width=[[item.width]]>
+              </rux-timeline-region>
+            </li>
+          </template>
         <ol>
       </div>
-
       `;
   }
   constructor() {
@@ -83,38 +80,17 @@ export class RuxTimelineTrack extends PolymerElement {
       0
     );
 
-    /* var self = this;
-    for (let i = 0; i < this.regions.length; i++) {
-      const _regionDuration =
-        self.regions[i].endTime.getTime() - self.regions[i].startTime.getTime();
-      self.regions[i].width =
-        _regionDuration * this._base.offsetWidth / this.duration;
-
-      self.regions[i].left =
-        (self.regions[i].startTime.getTime() - today.getTime()) *
-        this._base.offsetWidth /
-        this.duration;
-    } */
-
     this.regions.forEach((region, i) => {
       const _regionDuration =
         region.endTime.getTime() - region.startTime.getTime();
       region.width = _regionDuration * this._base.offsetWidth / this.duration;
-
-      region.label = new Date().getSeconds();
 
       region.left =
         (region.startTime.getTime() - today.getTime()) *
         this._base.offsetWidth /
         this.duration;
 
-      this.regions[i].left = region.left;
-      this.regions[i].width = region.width;
-      this.notifyPath("regions.0.left");
-      this.notifyPath("regions.1.left");
-      this.notifyPath("regions.2.left");
-      this.notifyPath("regions.3.left");
-      this.notifyPath("regions.4.left");
+      this.notifyPath("regions");
     });
   }
 }
