0110d8c6e60a14444ee4c9e053e3690be9c47353
Duncan McAlester <duncan@rocketcom.com>
2018-03-22 10:29:00 -0700

    Updated notifications from component branch

--- src/astro-components/rux-notification/rux-notification.js
+++ src/astro-components/rux-notification/rux-notification.js
@@ -15,16 +15,28 @@ export class RuxNotification extends PolymerElement {
         type: Object,
         value: null
       },
-      target: {
-        type: Object
-      },
-      type: {
+      status: {
         type: String,
         value: "default"
       },
-      opened: {
+      push: {
         type: Boolean,
         value: false
+      },
+      target: {
+        type: String,
+        value: "local",
+        reflectToAttribute: true
+      },
+      closeAfter: {
+        type: Number,
+        value: false
+      },
+      opened: {
+        type: Boolean,
+        value: false,
+        reflectToAttribute: true,
+        observer: "_buffer"
       }
     };
   }
@@ -33,61 +45,97 @@ export class RuxNotification extends PolymerElement {
       <link rel="stylesheet" href="src/astro-components/rux-notification/rux-notification.css">
       
       <div class="rux-notification__message">[[message]]</div>
+      <!-- <rux-button type="icon" icon="default:close" on-click="_onClick"></rux-button> //-->
+      <a on-click="_onClick"><rux-icon icon="default:close" color="#fff" size="small"></rux-icon></a>
       `;
   }
   constructor() {
     super();
+
+    this.buffer;
+    this.container;
+    this._closeAfter;
   }
   connectedCallback() {
     super.connectedCallback();
 
-    // console.log("root", this.parentNode);
+    // make sure close after is a reasonable time frame.
+    if (this.closeAfter && this.closeAfter < 10) {
+      this.closeAfter *= 1000;
+    } else if (this.closeAfter > 10 && this.closeAfter < 2000) {
+      this.closeAfter = 3000;
+    }
 
-    // const _container = document.createElement("div");
-    // _container.classList.add("notification-container");
-    // _container.style.position = "absolute";
-    // _container.style.backgroundColor = "red";
-    // _container.style.height = "60px";
-    // _container.style.width = "100%";
-    // _container.style.top = "0";
-    // _container.style.left = "0";
-    // _container.style.width = "100%";
-    // _container.style.display = "block";
+    const _parent = this.parentNode;
 
-    // this.parentNode.insertBefore(_container, this.parentNode.firstChild);
+    // if the notification is attached to a local object then make sure it has a position set
+    // so the notifcation gets associated correctly. Also ensure the overflow is set to hidden
+    // NOTE: this is a very temporary solution. Messing with another developers position/overflow
+    // undoubtedly will have unintended consequences
+    if (this.target === "local") {
+      // if the parent element has a position of static or has no position set, then set it
+      if (!_parent.style.position || _parent.style.position === "static") {
+        _parent.style.position = "relative";
+      }
+      _parent.style.overflow = "hidden";
+    }
 
-    // const _buffer = document.createElement("div");
-    // _buffer.classList.add("notification-buffer");
-    // _buffer.style.position = "relative";
-    // _buffer.style.outline = "1px solid green";
-    // _buffer.style.webkitBorderTopRightRadius = "60px";
-    // _buffer.style.height = "60px";
-    // _buffer.style.width = "100%";
-    // _buffer.style.marginLeft = "0";
-    // _buffer.style.display = "block";
-    // _buffer.style.zIndex = 10;
+    // if the notification has the push param set, then insert some stuff
+    if (this.push) {
+      this.container = document.createElement("div");
+      this.buffer = document.createElement("div");
+      this.styleSheet = document.createElement("style");
+      this.styleSheet.innerHTML = `
+      .notification-container {
+				display: none;
+				height: 60px;
+				background-color: transparent;
+				position: absolute;
+				top: 0;
+				left: 0;	
+			}
+			
+			.notification-buffer {
+				height: 60px;
+				margin-top: -60px;
+				position: relative;
+				transition: all 0.5s ease;
+				width: 0;
+			}
 
-    // this.parentNode.insertBefore(_buffer, this.parentNode.firstChild);
+			.notification-buffer.show {
+				margin-top: 0;
+			}
+      `;
 
-    // console.log("_container.offsetTop", _container.offsetTop);
-    // console.log("_buffer.offsetTop", _buffer.top);
-
-    // if (!this.parentNode.style.position) {
-    //   this.parentNode.style.position = "relative";
-    // }
-
-    // let _firstElem = this.parentNode.childNodes[1];
-    // console.log("_firstElem", _firstElem);
-    // let _container = this.parentNode.insertBefore(
-    //   _firstElem,
-    //   document.createElement("div")
-    // );
-
-    // console.log("root", this.parentNode.style.position);
-    // this.parentNode.css('position')
+      this.container.classList.add("notification-container");
+      this.buffer.classList.add("notification-buffer");
+      _parent.appendChild(this.styleSheet);
+      _parent.insertBefore(this.container, _parent.firstChild);
+      _parent.insertBefore(this.buffer, _parent.firstChild);
+    }
   }
   disconnectedCallback() {
     super.disconnectedCallback();
+  }
+
+  _buffer() {
+    // if the notification has a closeAfter value start a timeout
+    if (this.closeAfter && this.opened) {
+      this._closeAfter = setTimeout(() => {
+        this.opened = false;
+      }, this.closeAfter);
+    }
+
+    // if this is a push down notification toggle the buffer element
+    if (this.buffer) {
+      this.buffer.classList.toggle("show");
+    }
+  }
+
+  _onClick(e) {
+    clearTimeout(this._closeAfter);
+    this.opened = false;
   }
 }
 customElements.define("rux-notification", RuxNotification);
